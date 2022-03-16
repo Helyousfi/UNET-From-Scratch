7e0ab4daf31e08aa9ac362ee108f522e4f3f08c5
Hamza EL Yousfi <Hamza.El_Yousfi@bordeaux-inp.fr>
2022-03-16 20:24:44 +0100

    Model architecture

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class convLayers(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(self, convLayers).__init__()
+        
+
+
+class ModelArchitecture(nn.Module):
+    def __init__(self, in_channels = 3):
+        super(self, ModelArchitecture)
